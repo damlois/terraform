7558b8da59fadc6d5ba4b99db823fc2b648c97fc
damlois <lois0168@gmail.com>
2019-02-14 14:26:02 +0100

    first commit

--- model.js
+++ model.js
@@ -15,7 +15,6 @@ const UserSchema = new Schema({
         type: String,
         required: (true, 'email is required'),
         unique: true,
-        //match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     },
     password: {
         type: String,
